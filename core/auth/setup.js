bc0e3692f1d6634597755dc782998f67b951aaef
Cameron Messinides <CMessinides@users.noreply.github.com>
2019-09-13 20:28:12 -0400

    fix(auth): ensure session cookies sent correctly in production (#6)

--- core/auth/setup.js
+++ core/auth/setup.js
@@ -28,6 +28,12 @@ function setupAuth(server) {
 			saveUninitialized: false
 		})
 	);
+	// If the app is hosted behind a proxy, this setting is required
+	// to ensure that cookies with 'secure' set to true are properly
+	// sent to the server when Auth0 redirects the user to the /callback
+	// route. See https://github.com/auth0/passport-auth0/issues/70#issuecomment-480771614
+	// for more details.
+	isDev && server.set("trust proxy", 1);
 
 	// Set up authentication through Auth0
 	const passport = require("passport");
